11c31976a4fd95140d82edf28adb81a514a753b6
lxh027 <992874705@qq.com>
2020-10-19 12:16:13 +0800

    update: test

--- app/api/controller/TestController.go
+++ app/api/controller/TestController.go
@@ -2,16 +2,25 @@ package controller
 
 import (
 	"OnlineJudge/app/api/model"
+	"OnlineJudge/app/api/validate"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 
 func Test(c *gin.Context)  {
 
 	var authModel = model.Authority{}
+	var authValidate = validate.AuthorityValidate
 
-	auths, err := authModel.GetAllAuthority()
+	if res, err := authValidate.Validate(c, "ind"); !res {
+		log.Println(err.Error())
+		return
+	}
+	id, err:= strconv.ParseUint(c.Query("id"), 10, 64)
+	auths, err := authModel.GetAuthorityByID(id)
 	if err != nil {
 		c.JSON(http.StatusOK, gin.H{
 			"msg": "ERROR",
