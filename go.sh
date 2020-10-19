883d452e20768e104a2a079c79aab85d0282b23e
lxh027 <992874705@qq.com>
2020-10-19 12:10:47 +0800

    update: 导入auth包

--- go.sh
+++ go.sh
@@ -4,3 +4,4 @@ go get github.com/gin-gonic/gin
 go get github.com/kardianos/govendor
 go get github.com/gin-contrib/sessions
 go get github.com/gin-contrib/sessions/redis
+go get github.com/gookit/validate
