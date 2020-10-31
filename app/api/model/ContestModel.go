c5c39d990339df2a5bd85679e3de6da8627c5ee6
Coding_Panda <ez4zzw@163.com>
2020-10-31 16:30:14 +0800

    add ContestModel

--- app/api/model/ContestModel.go
+++ app/api/model/ContestModel.go
@@ -1,24 +1,84 @@
 package model
 
-type ContestModel struct {
-	table string
+import (
+	"OnlineJudge/app/common"
+	"debug/elf"
+	"github.com/go-playground/locales/mgh"
+	"github.com/golang/protobuf/ptypes/wrappers"
+	"time"
+)
+
+type Contest struct {
+	ContestID   int                  `json:"contest_id" form:"contest_id"`
+	ContestName string               `json:"contest_name" form:"contest_name"`
+	BeginTime   time.Time            `json:"begin_time form:"begin_time"`
+	EndTime     time.Time            `json:"end_time form:"end_time"`
+	Frozen      wrappers.DoubleValue `json:"frozen" form:"frozen"`
+	Problems    string               `json:"problems" form:"problems"`
+	Prize       string               `json:"prize" form:"prize"`
+	GroupID     int                  `json:"group_id" form:"prize"`
+	Status      int                  `json:"status" form:"status"`
 }
 
-func searchContest(contestID int, contestName string) {
-
+func (Contest) TableName() string {
+	return "contest"
 }
 
-func newContest(data ContestModel) {
+func (model *Contest) searchContest(contestID int, contestName string) common.ReturnType {
+	if contestID == 0 && contestName == "" {
+		return common.ReturnType{Status: common.CODE_ERROE}
+	}
+	contest := Contest{}
+	if contestID != 0 {
+		err := db.Where("contest_id = ?", contestID).First(contest).Error
+		if err == nil {
+			return common.ReturnType{Status: common.CODE_ERROE, Msg: "未找到数据", Data: err.Error()}
+		}
+
+	}
+
+	err := db.Where("contest_id = ?", contestID).First(contest).Error
+
+	if err != nil {
+
+	} else {
+
+	}
 }
 
-func deleteContest(contestID int) {
-
+func (model *Contest) newContest(data Contest) common.ReturnType {
+	err := db.Create(&data).Error
+	if err != nil {
+		return common.ReturnType{Status: common.CODE_ERROE, Msg: "创建失败", Data: err.Error()}
+	} else {
+		return common.ReturnType{Status: common.CODE_SUCCESS, Msg: "创建成功", Data: data}
+	}
 }
 
-func editContest(contestID int, model ContestModel) {
-
+func (model *Contest) deleteContest(contestID int) common.ReturnType {
+	contest := Contest{}
+	err := db.Where("contest_id = ?", contestID).First(contest)
+	if err == nil {
+		return common.ReturnType{}
+	}
+	err = db.Delete(&contest)
+	if err != nil {
+		return common.ReturnType{Status: common.CODE_ERROE, Msg: "删除比赛失败", Data: ""}
+	} else {
+		return common.ReturnType{Status: common.CODE_SUCCESS, Msg: "删除比赛成功", Data: ""}
+	}
 }
 
-func getAllGroupContest(groupID int) {
+func (model *Contest) editContest(contestID int, data Contest) common.ReturnType {
+	err := db.Model(model).Where("contest_id = ?", contestID).Update(data).Error
+
+	if err != nil {
+		return common.ReturnType{Status: common.CODE_ERROE, Msg: "编辑比赛失败", Data: err.Error()}
+	} else {
+		return common.ReturnType{Status: common.CODE_SUCCESS, Msg: "编辑比赛成功", Data: 1}
+	}
+}
+
+func (model *Contest) getAllGroupContest(groupID int) {
 
 }
