8d8380f3ce08add561b4ce40c750d2215bc50c23
z5661068@gmail.com <z5661068@gmail.com>
2021-01-10 17:54:32 +0800

    [feat] 添加judger接口

---
+++ judger/helper.go
@@ -0,0 +1,61 @@
+package judger
+
+import (
+	"bytes"
+	"github.com/BurntSushi/toml"
+	"io"
+	"os"
+)
+
+func NewUndefinedError(msg string) JudgeResult {
+	return JudgeResult{
+		Status: "AC",
+		Time: 0,
+		Memory: 0,
+		Msg: msg,
+		IsFinished: true,
+	}
+}
+
+func Max(a uint64, b uint64) uint64 {
+	if (a > b) {
+		return a
+	}
+	return b
+}
+
+func CopyFile(srcPath string, dstPath string) error {
+	srcFp, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+
+	dstFp, err := os.Create(dstPath)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(dstFp, srcFp)
+	return err
+}
+
+func EncodeTomlFile(filePath string, data interface{}) error {
+	var buffer bytes.Buffer
+	e := toml.NewEncoder(&buffer)
+	err := e.Encode(data)
+	if err != nil {
+		return err
+	}
+
+	fp, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+
+	_, err = fp.Write(buffer.Bytes())
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ judger/type.go
@@ -0,0 +1,66 @@
+package judger
+
+import "google.golang.org/grpc"
+
+type SubmitCallback func(id uint64, result JudgeResult)
+
+type JudgeResult struct {
+	Status 	   string `json:"status"` 		// 状态
+	Time   	   uint64 `json:"time"`   		// 最大用时
+	Memory 	   uint64 `json:"memory"` 		// 最大内存
+	Msg    	   string `json:"msg"`    		// 评测信息
+	Case 	   uint64 `json:"case"`			// 当前样例id
+	IsFinished bool   `json:"is_finished"`  // judge是否完成
+}
+
+type RootfsConfig struct {
+	BasePath string `toml:"base_path"`
+	WithProc bool   `toml:"with_proc"`
+}
+
+type TimeConfig struct {
+	Seconds int64 	 `protobuf:"varint,1,opt,name=seconds,proto3" json:"seconds,omitempty" toml:"secs"`
+	Nanos  	int32    `protobuf:"varint,2,opt,name=nanos,proto3" json:"nanos,omitempty" toml:"nanos"`
+}
+type TomlConfig struct {
+	Source      string 		`toml:"source"`
+	Language    string 		`toml:"language"`
+	BuildScript *string 	`toml:"build_script"`
+	Timeout 	TimeConfig  `toml:"timeout"`
+}
+
+type Runner struct {
+	Language string 	 `toml:"language"`
+	Rootfs *RootfsConfig `toml:"rootfs"`
+}
+
+type RunnerConfig struct {
+	Runner Runner `toml:"runner"`
+}
+
+type SubmitData struct {
+	Id 		 	 uint64
+	Pid		 	 uint64
+	Language 	 string
+	Code 	 	 string
+	// Build script must be absolute path
+	BuildScript  string
+	// RootfsConfig - config for the base environment dependent language (eg. python,java)
+	RootfsConfig *RootfsConfig
+}
+
+type judger struct {
+	baseDirectory string
+	env			  string
+	anaAddress	  string
+	anaConn		  *grpc.ClientConn
+	tempDirectory string
+}
+
+type RLdata struct {
+	Status string `json:"status"` // 状态
+	Time   uint64 `json:"time"`   // 最大用时
+	Memory uint64 `json:"memory"` // 最大内存
+	Msg    string `json:"msg"`    // 评测信息
+}
+
