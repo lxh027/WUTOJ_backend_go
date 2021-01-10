d5d85439190e6173930e158b36253a1312221ec0
lxh027 <992874705@qq.com>
2021-01-10 13:35:23 +0800

    注册后台路由

--- routes/router.go
+++ routes/router.go
@@ -1,8 +1,10 @@
 package routes
 
 import (
-	"OnlineJudge/app/api/controller"
+	 apiController "OnlineJudge/app/api/Controller"
+	 panelController "OnlineJudge/app/panel/Controller"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Routes(router *gin.Engine)  {
@@ -10,11 +12,59 @@ func Routes(router *gin.Engine)  {
 	// api
 	api := router.Group("/api")
 	{
-		api.GET("/", controller.Index)
-		api.POST("/register", controller.Register)
+		api.GET("/hello", apiController.Index)
+		api.POST("/register", apiController.Register)
 
-		api.POST("/do_login", controller.DoLogin)
-		api.POST("/do_logout", controller.DoLogout)
+		api.POST("/do_login", apiController.DoLogin)
+		api.POST("/do_logout", apiController.DoLogout)
 	}
 
+	panel := router.Group("/panel")
+	{
+		user := panel.Group("/user")
+		{
+			user.POST("/getAllUser", panelController.GetAllUser)
+			user.POST("/getUserByID", panelController.GetUserByID)
+			user.POST("/register", panelController.Register)
+			user.POST("/login", panelController.Login)
+			user.POST("/logout", panelController.Logout)
+			user.POST("/getUserInfo", panelController.GetUserInfo)
+			user.POST("/updateUser", panelController.UpdateUser)
+			user.POST("/deleteUser", panelController.DeleteUser)
+			user.POST("/setAdmin", panelController.SetUserAdmin)
+		}
+
+		role := panel.Group("/role")
+		{
+			role.POST("/getAllRole", panelController.GetAllRole)
+			role.POST("/getRoleByID", panelController.GetRoleByID)
+			role.POST("/addRole", panelController.AddRole)
+			role.POST("/deleteRole", panelController.DeleteRole)
+			role.POST("/updateRole", panelController.UpdateRole)
+		}
+
+		userRole := panel.Group("/userRole")
+		{
+			userRole.POST("/getUserRolesList", panelController.GetUserRolesList)
+			userRole.POST("/addUserRoles", panelController.AddUserRoles)
+			userRole.POST("/deleteUserRoles", panelController.DeleteUserRoles)
+		}
+
+		auth := panel.Group("/auth")
+		{
+			auth.POST("/getAllAuth", panelController.GetAllAuth)
+			auth.POST("/getParentAuth", panelController.GetParentAuth)
+			auth.POST("/addAuth", panelController.AddAuth)
+			auth.POST("/deleteAuth", panelController.DeleteAuth)
+			auth.POST("/getAuthByID", panelController.GetAuthByID)
+			auth.POST("/updateAuth", panelController.UpdateAuth)
+		}
+		roleAuth := panel.Group("roleAuth")
+		{
+			roleAuth.POST("/getRoleAuthsList", panelController.GetRoleAuthsList)
+			roleAuth.POST("/addRoleAuths", panelController.AddRoleAuths)
+			roleAuth.POST("/deleteRoleAuths", panelController.DeleteRoleAuths)
+		}
+	}
+	router.StaticFS("/admin/", http.Dir("./web"))
 }
