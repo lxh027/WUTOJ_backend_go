e8ff4bcee69e0eca2b2b8560d70a0b175d916006
Coding_Panda <55881927+EZ4ZZW@users.noreply.github.com>
2021-03-18 22:35:35 +0800

    add: Problem

--- app/api/controller/ProblemController.go
+++ app/api/controller/ProblemController.go
@@ -1,23 +1,45 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"OnlineJudge/app/api/model"
+	"OnlineJudge/app/common"
+	"OnlineJudge/app/common/validate"
+	"OnlineJudge/app/helper"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func GetAllProblems(c *gin.Context) {
+
+	problemModel := model.Problem{}
+	res := problemModel.GetAllProblems()
+	c.JSON(http.StatusOK, helper.ApiReturn(res.Status, res.Msg, res.Data))
+	return
 
 }
 
 func GetProblemByID(c *gin.Context) {
+	problemValidate := validate.ProblemValidate
+	problemModel := model.Problem{}
 
-}
+	var problemJson model.Problem
 
-func AddProblem(c *gin.Context) {
+	if err := c.ShouldBind(&problemJson); err != nil {
+		c.JSON(http.StatusOK, helper.ApiReturn(common.CodeError, "绑定数据模型失败", err.Error()))
+		return
+	}
 
-}
+	problemMap := helper.Struct2Map(problemJson)
+	if res, err := problemValidate.ValidateMap(problemMap, "findByID"); !res {
+		c.JSON(http.StatusOK, helper.ApiReturn(common.CodeError, err.Error(), 0))
+		return
+	}
 
-func EditProblem(c *gin.Context) {
-
+	res := problemModel.GetProblemByID(int(problemJson.ProblemID))
+	c.JSON(http.StatusOK, helper.ApiReturn(res.Status, res.Msg, res.Data))
+	return
 }
 
 func SearchProblem(c *gin.Context) {
-
+	// to do
 }
--- app/api/model/ProblemModel.go
+++ app/api/model/ProblemModel.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"OnlineJudge/app/common"
+	"OnlineJudge/app/helper"
+)
+
 type Problem struct {
 	ProblemID    uint    `json:"problem_id" form:"problem_id"`
 	Title        string  `json:"title" form:"title" `
@@ -16,4 +21,71 @@ type Problem struct {
 	Tag          string  `json:"tag" form:"tag"`
 	Path         string  `json:"path" form:"path"`
 	Status       int     `json:"status" form:"status"`
+}
+
+func (model *Problem) GetAllProblems() helper.ReturnType {
+	var Problems []Problem
+	var count int
+
+	db.Model(&Problem{}).Count(&count)
+
+	err := db.Find(&Problems).Error
+
+	if err != nil {
+		return helper.ReturnType{Status: common.CodeError, Msg: "查询失败", Data: err.Error()}
+	} else {
+		return helper.ReturnType{Status: common.CodeSuccess, Msg: "查询成功",
+			Data: map[string]interface{}{
+				"data":  Problems,
+				"count": count,
+			},
+		}
+	}
+
+}
+
+func (model *Problem) AddProblem(newProblem Problem) helper.ReturnType { //jun
+	err := db.Create(&newProblem).Error
+
+	if err != nil {
+		return helper.ReturnType{Status: common.CodeError, Msg: "创建失败", Data: err.Error()}
+	} else {
+		var idProblem Problem
+		db.First(&idProblem, newProblem)
+		return helper.ReturnType{Status: common.CodeSuccess, Msg: "创建成功", Data: idProblem.ProblemID}
+	}
+}
+
+func (model *Problem) UpdateProblem(problemID int, updateProblem Problem) helper.ReturnType {
+	err := db.Model(&Problem{}).Where("problem_id = ?", problemID).Update(updateProblem).Error
+
+	if err != nil {
+		return helper.ReturnType{Status: common.CodeError, Msg: "更新失败", Data: false}
+	} else {
+		return helper.ReturnType{Status: common.CodeSuccess, Msg: "更新成功", Data: true}
+	}
+}
+
+func (model *Problem) GetProblemByID(id int) helper.ReturnType {
+	var problem Problem
+
+	err := db.Where("problem_id = ?", id).First(&problem).Error
+
+	if err != nil {
+		return helper.ReturnType{Status: common.CodeError, Msg: "查询失败", Data: err.Error()}
+	} else {
+		return helper.ReturnType{Status: common.CodeSuccess, Msg: "查询成功", Data: problem}
+	}
+}
+
+func (model *Problem) GetProblemByTitle(title string) helper.ReturnType {
+	var problem Problem
+
+	err := db.Where("title = ?", title).First(&problem).Error
+
+	if err != nil {
+		return helper.ReturnType{Status: common.CodeError, Msg: "查询失败", Data: err.Error()}
+	} else {
+		return helper.ReturnType{Status: common.CodeSuccess, Msg: "查询成功", Data: problem}
+	}
 }
