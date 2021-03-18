a4952cbaef9d39f87e423bf6cac48b06ccd5e7a6
Coding_Panda <55881927+EZ4ZZW@users.noreply.github.com>
2021-03-18 22:36:01 +0800

    add: ReplyValidate

---
+++ app/common/validate/ReplyValidate.go
@@ -0,0 +1,23 @@
+package validate
+
+import "C"
+import "OnlineJudge/app/helper"
+
+var ReplyValidate helper.Validator
+
+func init() {
+	rules := map[string]string{
+		"id":         "required",
+		"discuss_id": "required",
+		"user_id":    "required",
+		"content":    "required",
+		"time":       "required",
+	}
+
+	scenes := map[string][]string{
+		"add": {"discuss_id", "user_id", "content", "time"},
+	}
+
+	ReplyValidate.Rules = rules
+	ReplyValidate.Scenes = scenes
+}
