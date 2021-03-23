5bf1b89ce2c297ed90612e68805dcdf7dfaf44ad
lxh027 <992874705@qq.com>
2021-03-23 22:05:37 +0800

    pref: 更改checkContest到Contest Controller

--- app/api/controller/CommonController.go
+++ app/api/controller/CommonController.go
@@ -1,39 +1,11 @@
 package controller
 
 import (
-	"OnlineJudge/app/api/model"
 	"OnlineJudge/app/common"
-	"OnlineJudge/app/common/validate"
 	"OnlineJudge/app/helper"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
-
-func CheckContest(c *gin.Context) {
-	session := sessions.Default(c)
-	UserID := int(session.Get("user_id").(uint))
-	contestUserModel := model.ContestUser{}
-	contestValidate := validate.ContestValidate
-
-	var contestJson model.Contest
-	if err := c.ShouldBindUri(&contestJson); err != nil {
-		c.JSON(http.StatusOK, helper.ApiReturn(common.CodeError, "数据模型绑定错误", err.Error()))
-		return
-	}
-
-	contestMap := helper.Struct2Map(contestJson)
-	if res, err := contestValidate.ValidateMap(contestMap, "join"); !res {
-		c.JSON(http.StatusOK, helper.ApiReturn(common.CodeError, err.Error(), 0))
-		return
-	}
-
-	res := contestUserModel.CheckUserContest(UserID, contestJson.ContestID)
-
-	c.JSON(http.StatusOK, helper.ApiReturn(res.Status, res.Msg, res.Data))
-	return
-
-}
 
 func GetUserIdFromSession(c *gin.Context) uint {
 	session := sessions.Default(c)
--- app/api/controller/ContestController.go
+++ app/api/controller/ContestController.go
@@ -6,6 +6,7 @@ import (
 	"OnlineJudge/app/common/validate"
 	"OnlineJudge/app/helper"
 	"fmt"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -54,15 +55,7 @@ func GetContest(c *gin.Context) {
 		return
 	} else {
 		res = contestModel.GetContestByName(ContestID)
-		if res.Status != common.CodeError {
-			c.JSON(http.StatusOK, helper.ApiReturn(res.Status, res.Msg, res.Data))
-			return
-		} else {
-			c.JSON(http.StatusOK, helper.ApiReturn(common.CodeError, "查找失败", ""))
-			return
-		}
 		c.JSON(http.StatusOK, helper.ApiReturn(res.Status, res.Msg, res.Data))
-		return
 	}
 
 }
@@ -181,4 +174,29 @@ func SearchContest(c *gin.Context) {
 		c.JSON(http.StatusOK, helper.ApiReturn(common.CodeError, "数据模型绑定错误", err.Error()))
 		return
 	}
+}
+
+func checkContest(c *gin.Context) {
+	session := sessions.Default(c)
+	UserID := int(session.Get("user_id").(uint))
+	contestUserModel := model.ContestUser{}
+	contestValidate := validate.ContestValidate
+
+	var contestJson model.Contest
+	if err := c.ShouldBindUri(&contestJson); err != nil {
+		c.JSON(http.StatusOK, helper.ApiReturn(common.CodeError, "数据模型绑定错误", err.Error()))
+		return
+	}
+
+	contestMap := helper.Struct2Map(contestJson)
+	if res, err := contestValidate.ValidateMap(contestMap, "join"); !res {
+		c.JSON(http.StatusOK, helper.ApiReturn(common.CodeError, err.Error(), 0))
+		return
+	}
+
+	res := contestUserModel.CheckUserContest(UserID, contestJson.ContestID)
+
+	c.JSON(http.StatusOK, helper.ApiReturn(res.Status, res.Msg, res.Data))
+	return
+
 }
