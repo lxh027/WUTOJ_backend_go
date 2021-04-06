c0ec8402eaeaf5dfb654305f744db49dd359bef9
EZ4ZZW <zhiwayzhang@outlook.com>
2021-04-06 10:51:51 +0800

    update: 更新评测机Config

--- config/judge.go
+++ config/judge.go
@@ -1,12 +1,12 @@
 package config
 
-func GetJudgeConfig()map[string]interface{}  {
+func GetJudgeConfig() map[string]interface{} {
 	judgeConfig := make(map[string]interface{})
 
-	judgeConfig["env"] = "dev"
+	judgeConfig["env"] = ""
 	judgeConfig["address"] = "127.0.0.1:8800"
-	judgeConfig["base_dir"] = "/home/acmwhut/data"
-	judgeConfig["tmp_dir"] = "/tmpdir"
+	judgeConfig["base_dir"] = "/home/acmwhut/data/dev"
+	judgeConfig["tmp_dir"] = "/home/ana_tmpdir"
 
 	return judgeConfig
 }
