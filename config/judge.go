68dd5c8f6d54cfd64594b0a047fce9805b784958
baka233 <33756229+baka233@users.noreply.github.com>
2021-05-21 20:30:37 +0800

    [fix] fir wrong judge config

--- config/judge.go
+++ config/judge.go
@@ -23,22 +23,22 @@ func GetLangConfigs() []LangConfig {
 	langBuildPath := []string {
 		"",
 		"",
+		"/java.openjdk10/build.sh",
 		"/python.cpython3.6/build.sh",
-		"/java.openjdk-10/build.sh",
 	}
 
 	langRunnerConfig := []string {
 		"/c.gcc/runner.toml",
 		"/cpp.g++/runner.toml",
+		"/java.openjdk10/runner.toml",
 		"/python.cpython3.6/runner.toml",
-		"/java.openjdk-10/runner.toml",
 	}
 	
 	langConfigs := []LangConfig{
 		{"c.gcc", "", langBasePath + langRunnerConfig[0]},
 		{"cpp.g++", "", langBasePath + langRunnerConfig[1]},
-		{"python.cpython3.6", langBasePath + langBuildPath[2], langBasePath + langRunnerConfig[2]},
-		{"java.openjdk-10", langBasePath + langBuildPath[3], langBasePath + langRunnerConfig[3]},
+		{"java.openjdk10", langBasePath + langBuildPath[2], langBasePath + langRunnerConfig[2]},
+		{"python.cpython3.6", langBasePath + langBuildPath[3], langBasePath + langRunnerConfig[3]},
 	}
 	return langConfigs
 }
