6eea80594305477505cfbc1e5bab03607d7ca666
lxh027 <992874705@qq.com>
2021-05-24 22:32:05 +0800

    fix: rank

--- app/api/controller/SubmitController.go
+++ app/api/controller/SubmitController.go
@@ -37,7 +37,9 @@ func Submit(c *gin.Context) {
 		return
 	}
 
-	now := time.Now().Unix()
+	format := "2006-01-02 15:04:05"
+	now, _ := time.Parse(format, time.Now().Format(format))
+
 	//interval := config.GetWutOjConfig()["interval_time"].(int)
 	redisStr := "user_last_submit" + strconv.Itoa(int(userID.(uint)))
 	//if value, err := db_server.GetFromRedis(redisStr); err == nil {
@@ -75,6 +77,8 @@ func Submit(c *gin.Context) {
 		return
 	}
 
+
+
 	newSubmit := model.Submit{
 		UserID:     userID.(uint),
 		Nick:       GetUserNickFromSession(c),
@@ -83,7 +87,7 @@ func Submit(c *gin.Context) {
 		ProblemID:  submitJson.ProblemID,
 		ContestID:  submitJson.ContestID,
 		Status:     "Judging",
-		SubmitTime: time.Now(),
+		SubmitTime: now,
 	}
 
 	resp := submitModel.AddSubmit(&newSubmit)
