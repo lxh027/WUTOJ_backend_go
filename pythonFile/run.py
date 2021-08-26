9ca32243ecba76d8f95bfd74b1c2583876aeb132
UncleMadeleine <59909003+UncleMadeleine@users.noreply.github.com>
2021-08-27 00:28:59 +0800

    nothing

--- pythonFile/run.py
+++ pythonFile/run.py
@@ -4,6 +4,7 @@ import sys
 # import getpot
 wqy = sys.argv
 str = ""
+print("wqy")
 with open('../commit.log', 'r') as f:
 	wcr = f.readlines()
 flag = 1
@@ -18,12 +19,14 @@ for i in wcr:
 		continue
 	str += i.strip()+"\n"
 # f.close()
+print("wyh")
 data = {
 	"ToUserUid": int(wqy[1]),
 	"SendToType": 2,
 	"SendMsgType": "TextMsg",
 	"Content": "yzb又瞎push了什么怪东西进https://github.com/UncleMadeleine/WUTOJ_backend_go/commits/main：\n"+str
 }
+print("ljw")
 values = urllib.parse.urlencode(data).encode(encoding='UTF8')
 headers = {'Content-Type': 'application/json'}
 print(data)
