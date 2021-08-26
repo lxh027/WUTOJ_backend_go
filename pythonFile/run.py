9cfe5946f7d841ce2000a03e9a231ffbcbc75a13
UncleMadeleine <59909003+UncleMadeleine@users.noreply.github.com>
2021-08-27 00:53:08 +0800

    nothing5

--- pythonFile/run.py
+++ pythonFile/run.py
@@ -2,8 +2,8 @@ import urllib.request
 import json
 import sys
 # import getpot
-# wqy = sys.argv
-# str = ""
+wqy = sys.argv
+str = ""
 print("wqy")
 # with open('../commit.log', 'r') as f:
 # 	wcr = f.readlines()
@@ -19,20 +19,20 @@ print("wqy")
 # 		continue
 # 	str += i.strip()+"\n"
 # # f.close()
-# print("wyh")
-# data = {
-# 	"ToUserUid": int(wqy[1]),
-# 	"SendToType": 2,
-# 	"SendMsgType": "TextMsg",
-# 	"Content": "yzb又瞎push了什么怪东西进https://github.com/UncleMadeleine/WUTOJ_backend_go/commits/main：\n"+str
-# }
-# print("ljw")
-# values = urllib.parse.urlencode(data).encode(encoding='UTF8')
-# headers = {'Content-Type': 'application/json'}
-# print(data)
-# print(values)
-# print(json.dumps(data))
-# print(json.dumps(data).encode())
-# request = urllib.request.Request(
-# 	url=wqy[2], headers=headers, data=json.dumps(data).encode())
-# response = urllib.request.urlopen(request)
+print("wyh")
+data = {
+	"ToUserUid": int(wqy[1]),
+	"SendToType": 2,
+	"SendMsgType": "TextMsg",
+	"Content": "yzb又瞎push了什么怪东西进https://github.com/UncleMadeleine/WUTOJ_backend_go/commits/main：\n"+str
+}
+print("ljw")
+values = urllib.parse.urlencode(data).encode(encoding='UTF8')
+headers = {'Content-Type': 'application/json'}
+print(data)
+print(values)
+print(json.dumps(data))
+print(json.dumps(data).encode())
+request = urllib.request.Request(
+	url=wqy[2], headers=headers, data=json.dumps(data).encode())
+response = urllib.request.urlopen(request)
