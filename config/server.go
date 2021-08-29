592d76f43f148107d7a0dd880f4e6c4fb0226f39
lxh001 <xuhui.lou@outlook.com>
2021-08-29 09:57:47 +0800

    fix: revert files

--- config/server.go
+++ config/server.go
@@ -8,7 +8,7 @@ func init() {
 	serverConfig = make(map[string]interface{})
 
 	serverConfig["host"] = "0.0.0.0"
-	serverConfig["port"] = os.Getenv("env")
+	serverConfig["port"] = os.Getenv("server_port")
 
 	serverConfig["mode"] = "debug"
 }
