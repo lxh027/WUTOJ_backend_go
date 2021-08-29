e3ca28ba06fa03c8fa193517b935f50e0b72930d
lxh001 <xuhui.lou@outlook.com>
2021-08-29 09:41:41 +0800

    fix: revert files

--- config/database.go
+++ config/database.go
@@ -1,21 +1,18 @@
 package config
 
+import "os"
+
 var dbConfig map[string]interface{}
 
 func init() {
 	// init db config
 	dbConfig = make(map[string]interface{})
 
-	// dbConfig["hostname"] = os.Getenv("database_host")
-	dbConfig["hostname"] = "localhost"
-	// dbConfig["port"] = os.Getenv("database_port")
-	dbConfig["port"] = "3306"
-	// dbConfig["database"] = os.Getenv("database_name")
-	dbConfig["database"] = "online_judge"
-	// dbConfig["username"] = os.Getenv("database_user")
-	dbConfig["username"] = "root"
-	// dbConfig["password"] = os.Getenv("database_passwd")
-	dbConfig["password"] = "root"
+	dbConfig["hostname"] = os.Getenv("database_host")
+	dbConfig["port"] = os.Getenv("database_port")
+	dbConfig["database"] = os.Getenv("database_name")
+	dbConfig["username"] = os.Getenv("database_user")
+	dbConfig["password"] = os.Getenv("database_passwd")
 	dbConfig["charset"] = "utf8"
 	dbConfig["parseTime"] = "True"
 
--- config/server.go
+++ config/server.go
@@ -6,8 +6,8 @@ func init() {
 	serverConfig = make(map[string]interface{})
 
 	serverConfig["host"] = "0.0.0.0"
-	// serverConfig["port"] = os.Getenv("server_port")
 	serverConfig["port"] = "5000"
+
 	serverConfig["mode"] = "debug"
 }
 
