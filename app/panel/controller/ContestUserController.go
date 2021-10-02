61d8483b3c33c1e6b8eb37bdb0da0dd5cfc461b1
UncleMadeleine <59909003+UncleMadeleine@users.noreply.github.com>
2021-10-02 21:12:20 +0800

    eat(#41): 添加数据库事务

--- app/panel/controller/ContestUserController.go
+++ app/panel/controller/ContestUserController.go
@@ -31,9 +31,8 @@ func GetAllContestUsers(c *gin.Context) {
 
 //AddContestUsers 通过csv文件批量添加参赛选手
 func AddContestUsers(c *gin.Context) {
-	contestUserModel := model.ContestUser{}
 	userModel := model.User{}
-
+	var users []model.User
 	contestUserJSON := struct {
 		ContestID int `json:"contest_id" form:"contest_id"`
 	}{}
@@ -71,44 +70,29 @@ func AddContestUsers(c *gin.Context) {
 				return
 			}
 
-			//用于康康record
-			// fmt.Print(record)
-			// fmt.Printf("Record has %d columns.\n", len(record))
-			// city, _ := iconv.ConvertString(record[2], "gb2312", "utf-8")
-			// fmt.Printf("%s %s %s \n", record[0], record[1], city)
-
 			if len(record) != len(title) {
 				c.JSON(http.StatusOK, helper.BackendApiReturn(constants.CodeError, "文件读取出错", false))
 				return
 			}
+
 			var user model.User
+
 			//csv格式:0:teamID,1:realname,2:school,3:major,4:class,5:contact
+
 			user.Nick = "c_" + fmt.Sprintf("%d", contestUserJSON.ContestID) + "_" + record[0]
 			user.Realname = record[1]
 			user.School = record[2]
 			user.Major = record[3]
 			user.Class = record[4]
 			user.Contact = record[5]
-			res := userModel.AddUserWithoutCheckEMail(user)
-			if res.Msg != "创建成功" {
-				c.JSON(http.StatusOK, helper.BackendApiReturn(res.Status, res.Msg, res.Data))
-				return
-			}
-			var contestUser model.ContestUser
-			// var findUser model.User
-			contestUser.ContestID = contestUserJSON.ContestID
-			findUser := userModel.GetUserByNick(user.Nick)
-			user_to_id, ok := (findUser.Data).(model.User)
-			if !ok {
-				c.JSON(http.StatusOK, helper.BackendApiReturn(constants.CodeError, "数据库错误", false))
-			}
-			contestUser.UserID = user_to_id.UserID
-			res = contestUserModel.AddContestUser(contestUser)
-			if res.Msg != "参加比赛成功" {
-				c.JSON(http.StatusOK, helper.BackendApiReturn(res.Status, res.Msg, res.Data))
-				return
-			}
+			users = append(users, user)
 		}
+		res := userModel.AddUsersAndContestUsers(users, contestUserJSON.ContestID)
+		if res.Status != constants.CodeSuccess {
+			c.JSON(http.StatusOK, helper.BackendApiReturn(res.Status, res.Msg, res.Data))
+			return
+		}
+
 	} else {
 		c.JSON(http.StatusOK, helper.BackendApiReturn(constants.CodeError, "绑定数据模型失败", false))
 		return
